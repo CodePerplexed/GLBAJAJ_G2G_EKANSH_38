fd507c39c2df150043298a22940f6b7b1b9604d3
CodePerplexed <141564229+CodePerplexed@users.noreply.github.com>
2026-02-09 11:18:16 +0530

    Modify main function to calculate sum of two numbers
    Updated the program to read two integers from user input and print their sum.

--- sum.c
+++ sum.c
@@ -1,5 +1,9 @@
-#include<stdio.h>
-void main()
-{
-    printf("hello world");
+#include <stdio.h>
+
+void main() {
+    int a, b, c;
+    printf ("Enter two numbers.\n");
+    scanf ("%i%d", &a, &b);
+    c = a + b;
+    printf("The Sum of %d and %d = %d", a, b, c);
 }
