9140eade673146d41a542440c18e716f8cc9c420
CodePerplexed <141564229+CodePerplexed@users.noreply.github.com>
2026-03-23 12:00:41 +0530

    Program that prints alphabetical pattern
    Pattern:
    
    A
    B B
    C C C

---
+++ Pattern7.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    char i, j;
+    n = 'A' + 3;
+
+    // Printing Pattern
+    for(i = 'A'; i < n; i++)
+    {
+        for(j = 'A'; j <= i; j++)
+        {
+            printf("%c ", i);
+        }
+        printf("\n"); // prints on next line
+    }
+
+    return 0;
+}
