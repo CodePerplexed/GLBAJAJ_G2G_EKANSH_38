c7be194820f58246696d32c8d355a18c82864831
CodePerplexed <141564229+CodePerplexed@users.noreply.github.com>
2026-04-19 12:22:37 +0000

    Adding comments and implementing program by switch case

--- calculator.c
+++ calculator.c
@@ -5,19 +5,44 @@ int main()
 {
     int a, b, o;
     char c;
+
     printf("Enter two numbers and the operator respectively.\n");
-    scanf("%d%d %c",&a ,&b, &c);
-    if(c == '+')
-    o = a + b;
-    else if(c == '-')
-    o = a - b;
-    else if(c == '*')
-    o = a * b;
-    else if(c == '/')
-    o = a / b;
-    else if(c == '^')
-    o = pow(a, b);
+    scanf("%d%d %c",&a ,&b, &c); // space before %c avoids newline issue
     
+    switch (c)
+    {
+    case '+':
+    o = a + b;
     printf("Result = %d", o);
+    break;
+
+    case '-':
+    o = a - b;
+    printf("Result = %d", o);
+    break;
+
+    case '*':
+    o = a * b;
+    printf("Result = %d", o);
+    break;
+
+    case '/':
+    o = a / b;
+    printf("Result = %d", o);
+    break;
+
+    case '^':
+    o = pow(a, b);
+    printf("Result = %d", o);
+    break;
+
+    case '%':
+    o = a % b;
+    printf("Result = %d", o);
+    break;
+    
+    default:
+    printf("Invalid operator.");
+    }
     return 0;
 }
